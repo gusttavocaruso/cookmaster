65b4b4a0a010ac3fa8cb596c30c9afda1195947b
gustavo.caruso <gustavoalmeida26@hotmail.com>
2022-01-25 15:06:51 -0300

    add 70% test covered

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -17,6 +17,7 @@ before(async () => {
 });
 after(() => { mongodb.connect.restore() });
 
+
 describe('Testa endpoints para cadastro de usuários', () => {
   let response;
 
@@ -169,6 +170,13 @@ describe('Testa endpoints para login', () => {
       expect(response.body).to.have.property('token');
     })
 
+    it('O token deve ser correspondente ao e-mail', () => {
+      const { token } = response.body;
+      const payload = jwt.decode(token);
+
+      expect(payload.data.email).to.be.equals('karnal@gmail.com');
+    })
+
   });
 
   describe('Testa quando login é tentado com senha inválida', () => {
@@ -231,22 +239,111 @@ describe('Testa endpoints para login', () => {
 
   });
 
+  describe('Testa quando login é tentado com email inválido', () => {
+
+    before(async () => {
+      const userMock = connectionMock
+        .db('Cookmaster').collection('users');
+  
+      await userMock.insertOne({
+        name: 'Leandro Karnal',
+        email: 'karnal@gmail.com',
+        password: '1234567855',
+      });
+
+      response = await chai.request(app)
+        .post('/login')
+        .send({
+          email: 'karna@gmail.com', 
+          password: '1234567855',
+        });
+    });
+
+    it('Deve retornar o status 401', () => {
+      expect(response).to.have.status(401);
+    })
+
+    it('A requisição deve retornar a mensagem: Incorrect username or password', () => {
+      expect(response.body.message).to.be.equals('Incorrect username or password');
+    })
+
+  });
+
+  describe('Testa quando login é tentado com sem o campo email', () => {
+
+    before(async () => {
+
+      response = await chai.request(app)
+        .post('/login')
+        .send({
+          password: '1234567855',
+        });
+    });
+
+    it('Deve retornar o status 401', () => {
+      expect(response).to.have.status(401);
+    })
+
+    it('A requisição deve retornar a mensagem: All fields must be filled', () => {
+      expect(response.body.message).to.be.equals('All fields must be filled');
+    })
+
+  });
+
+  describe('Testa quando login é tentado com sem o campo password', () => {
+
+    before(async () => {
+
+      response = await chai.request(app)
+        .post('/login')
+        .send({
+          email: 'karna@gmail.com',
+        });
+    });
+
+    it('Deve retornar o status 401', () => {
+      expect(response).to.have.status(401);
+    })
+
+    it('A requisição deve retornar a mensagem: All fields must be filled', () => {
+      expect(response.body.message).to.be.equals('All fields must be filled');
+    })
+
+  });
+
 });
 
-/*
 describe('Testa endpoints para cadastro de receitas', () => {
   let response;
 
   describe('Testa quando a receita é cadastrada com sucesso', () => {
-    before(async () => {
-      response = await chai.request(app)
-        .post('/recipes')
-        .send({
-          name: 'Ovo mexido',
-          ingredients: 'Ovo, sal',
-          preparation: 'Bate o ovo, frita o ovo.',
-        });
-    });
+
+      before(async () => {
+        const userMock = connectionMock
+          .db('Cookmaster').collection('users');
+    
+        await userMock.insertOne({
+          name: 'Leandro Karnal',
+          email: 'karnal@gmail.com',
+          password: '1234567855',
+        })
+
+        const loginMock = await chai.request(app)
+          .post('/login')
+          .send({
+            email: 'karnal@gmail.com', 
+            password: '1234567855',
+          });
+
+        response = await chai.request(app)
+          .post('/recipes')
+          .set('authorization', loginMock.body.token)
+          .send({
+            name: 'Ovo mexido',
+            ingredients: 'Ovo, sal',
+            preparation: 'Bate o ovo, coloca sal, frita o ovo, mexendo-o.',
+          });
+      });
 
     it('Deve retornar o status 201', () => {
       expect(response).to.have.status(201);
@@ -258,5 +355,158 @@ describe('Testa endpoints para cadastro de receitas', () => {
 
   });
 
-})
-*/
+  describe('Testa quando receita tenta ser cadastrada com token inválido', () => {
+
+      before(async () => {
+        const userMock = connectionMock
+          .db('Cookmaster').collection('users');
+    
+        await userMock.insertOne({
+          name: 'Leandro Karnal',
+          email: 'karnal@gmail.com',
+          password: '1234567855',
+        })
+
+        const loginMock = await chai.request(app)
+          .post('/login')
+          .send({
+            email: 'karnal@gmail.com', 
+            password: '1234567855',
+          });
+
+        response = await chai.request(app)
+          .post('/recipes')
+          .set('authorization', 'abc')
+          .send({
+            name: 'Ovo mexido',
+            ingredients: 'Ovo, sal',
+            preparation: 'Bate o ovo, coloca sal, frita o ovo, mexendo-o.',
+          });
+      });
+
+    it('Deve retornar o status 401', () => {
+      expect(response).to.have.status(401);
+    })
+
+    it('A requisição deve retornar a mensagem: jwt malformed', () => {
+      expect(response.body.message).to.be.equals('jwt malformed');
+    })
+
+
+  });
+
+  describe('Testa quando receita tenta ser cadastrada sem o campo "name"', () => {
+
+      before(async () => {
+        const userMock = connectionMock
+          .db('Cookmaster').collection('users');
+    
+        await userMock.insertOne({
+          name: 'Leandro Karnal',
+          email: 'karnal@gmail.com',
+          password: '1234567855',
+        })
+
+        const loginMock = await chai.request(app)
+          .post('/login')
+          .send({
+            email: 'karnal@gmail.com', 
+            password: '1234567855',
+          });
+
+        response = await chai.request(app)
+          .post('/recipes')
+          .set('authorization', loginMock.body.token)
+          .send({
+            ingredients: 'Ovo, sal',
+            preparation: 'Bate o ovo, coloca sal, frita o ovo, mexendo-o.',
+          });
+      });
+
+    it('Deve retornar o status 400', () => {
+      expect(response).to.have.status(400);
+    })
+
+    it('A requisição deve retornar a mensagem: Invalid entries. Try again.', () => {
+      expect(response.body.message).to.be.equals('Invalid entries. Try again.');
+    })
+
+  });
+
+  describe('Testa quando receita tenta ser cadastrada sem o campo "ingredients"', () => {
+
+    before(async () => {
+      const userMock = connectionMock
+        .db('Cookmaster').collection('users');
+  
+      await userMock.insertOne({
+        name: 'Leandro Karnal',
+        email: 'karnal@gmail.com',
+        password: '1234567855',
+      })
+
+      const loginMock = await chai.request(app)
+        .post('/login')
+        .send({
+          email: 'karnal@gmail.com', 
+          password: '1234567855',
+        });
+
+      response = await chai.request(app)
+        .post('/recipes')
+        .set('authorization', loginMock.body.token)
+        .send({
+          name: 'Ovo mexido',
+          preparation: 'Bate o ovo, coloca sal, frita o ovo, mexendo-o.',
+        });
+    });
+
+    it('Deve retornar o status 400', () => {
+      expect(response).to.have.status(400);
+    })
+
+    it('A requisição deve retornar a mensagem: Invalid entries. Try again.', () => {
+      expect(response.body.message).to.be.equals('Invalid entries. Try again.');
+    })
+
+  });
+
+  describe('Testa quando receita tenta ser cadastrada sem o campo "preparation"', () => {
+
+    before(async () => {
+      const userMock = connectionMock
+        .db('Cookmaster').collection('users');
+  
+      await userMock.insertOne({
+        name: 'Leandro Karnal',
+        email: 'karnal@gmail.com',
+        password: '1234567855',
+      })
+
+      const loginMock = await chai.request(app)
+        .post('/login')
+        .send({
+          email: 'karnal@gmail.com', 
+          password: '1234567855',
+        });
+
+      response = await chai.request(app)
+        .post('/recipes')
+        .set('authorization', loginMock.body.token)
+        .send({
+          name: 'Ovo mexido',
+          ingredients: 'Ovo, sal',
+        });
+    });
+
+    it('Deve retornar o status 400', () => {
+      expect(response).to.have.status(400);
+    })
+
+    it('A requisição deve retornar a mensagem: Invalid entries. Try again.', () => {
+      expect(response.body.message).to.be.equals('Invalid entries. Try again.');
+    })
+
+  });
+
+});
